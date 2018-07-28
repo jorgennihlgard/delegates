a9e3027a49d740e87afddca4b4755a2df646d242
Jörgen Nihlgård <j.nihlgard@outlook.com>
2018-07-28 11:16:22 +0200

    done

--- delegates/Program.cs
+++ delegates/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace delegates
@@ -10,12 +11,28 @@ namespace delegates
     {
         static void Main(string[] args)
         {
-        }
+            MyClass m = new MyClass();
+            m.MyFunc(fun);
 
+            
+        }
+        public static void fun(int i)
+        {
+            Console.WriteLine(i);
+        }
 
     }
     class MyClass
     {
+        public delegate void CallDel(int i);
 
+        public void MyFunc(CallDel d)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(3000);
+                d(i);
+            }
+        }
     }
 }
